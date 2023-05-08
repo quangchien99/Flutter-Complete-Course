9f5a043fb8d4c448defb96e070d65a998caacfd2
Chien <phamquangchien170499@gmail.com>
2023-05-08 23:14:13 +0700

    Add data for meals screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals_screen.dart';
 import 'package:meals_app/widgets/category_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = availableMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
     // Navigator.of(context).push(route)
     Navigator.push(
         context,
         MaterialPageRoute(
-          builder: ((ctx) => MealsScreen(title: 'Some title', meals: [])),
+          builder: ((ctx) =>
+              MealsScreen(title: category.title, meals: filteredMeals)),
         ));
   }
 
@@ -34,7 +39,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context, category);
               },
             )
         ],
