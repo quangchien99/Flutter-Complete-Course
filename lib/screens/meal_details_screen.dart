085dadf2bd2eb856ae540808fc6fc029be9c986d
Chien <phamquangchien170499@gmail.com>
2023-05-09 14:47:48 +0700

    implement details screen

--- lib/screens/meal_details_screen.dart
+++ lib/screens/meal_details_screen.dart
@@ -11,11 +11,57 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Image.network(
-        meal.imageUrl,
-        width: double.infinity,
-        height: 300,
-        fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Image.network(
+            meal.imageUrl,
+            width: double.infinity,
+            height: 300,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          for (final ingredient in meal.ingredients)
+            Text(
+              ingredient,
+              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+          const SizedBox(
+            height: 24,
+          ),
+          Text(
+            'Steps',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          for (final step in meal.steps)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                step,
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onBackground,
+                    ),
+              ),
+            ),
+        ]),
       ),
     );
   }
