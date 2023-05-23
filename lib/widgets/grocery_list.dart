ecfe699ecfc656a7fb9363a109501f7a84a7a732
Chien <phamquangchien170499@gmail.com>
2023-05-23 21:48:45 +0700

    Handle delete item

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -72,10 +72,20 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
-  void _removeItem(GroceryItem item) {
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
     setState(() {
       _groceryItems.remove(item);
     });
+    final url = Uri.https('flutter-prep-d8416-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode > 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
   }
 
   @override
