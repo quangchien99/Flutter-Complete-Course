b67069bbfa1ba328b9fd738520d3dff9060f2f79
Chien <phamquangchien170499@gmail.com>
2023-05-24 15:42:27 +0700

    add river pod for user places

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fav_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
@@ -26,7 +27,7 @@ final theme = ThemeData().copyWith(
 );
 
 void main() {
-  runApp(const MyApp());
+  const ProviderScope(child: MyApp());
 }
 
 class MyApp extends StatelessWidget {
